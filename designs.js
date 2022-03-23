15966c50814d6f1ef9b96a4b457107824d705c83
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-03-22 23:56:07 -0400

    created event listener to add color to cell in grid

--- designs.js
+++ designs.js
@@ -1,10 +1,32 @@
+//DOM Elements -- elements from the HTML that need to be manipulated by the JS
 // Select color input
+const colorPicker= document.getElementById("colorPicker");
 // Select size input
+const rowNumber = document.getElementById("inputHeight");
+const columnNumber = document.getElementById("inputWidth");
+const pixelCanvas = document.getElementById("pixelCanvas");
+const grid = document.getElementById("sizePicker");
 
-// When size is submitted by the user, call makeGrid()
+// Creation of the actual grid table
+grid.addEventListener("submit", function(event){
+    event.preventDefault();
+    makeGrid();
+});
+//Print the color in the grid when color is chosen
+pixelCanvas.addEventListener('click', function(event){
+    event.target.style.backgroundColor = colorPicker.value;
+});
+
 
 function makeGrid() {
+   for (let i = 0; i < rowNumber.value; i++) {
+       const row = pixelCanvas.insertRow(0);
+       for (let j = 0; j < columnNumber.value; j++){
+           row.insertCell(0);       
+        }
+   }
 
 // Your code goes here!
+
 
 }
